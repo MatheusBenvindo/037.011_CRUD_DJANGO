22a27bdd1275a6bb5fbae65f1c320f7bd25454bc
MatheusBenvindo <mattbenvindo@gmail.com>
2026-05-18 17:44:48 -0300

    18.05.2026 - 2

--- Projeto_CRUD/inventory/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from inventory.models import Equipment
-
-# Create your views here.
-def equipments_view(request):
-    equipments = Equipment.objects.all()
-    
-    return render(request, "equipment.html", {"Equipment": equipments})
